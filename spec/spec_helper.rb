b61deb88df7a33105e496003ebf100a2ae1423b9
Abe Voelker <abe@abevoelker.com>
2017-02-26 21:29:26 -0600

    Remove ActiveSupport dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
-require "active_support"
 require File.expand_path("../../lib/forgetsy.rb", __FILE__)
 require "fakeredis/rspec"
 require "timecop"
+
+SECONDS_IN_DAY = 24 * 60 * 60
+DAY = SECONDS_IN_DAY
+WEEK = 7 * SECONDS_IN_DAY
+MONTH = 30 * DAY
 
 RSpec.configure do |c|
   c.before(:all) do
